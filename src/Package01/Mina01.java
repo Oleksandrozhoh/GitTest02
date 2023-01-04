33cea156db7784453345919329798da8cf17fd2a
mengliiii <menglivy@hotmail.com>
2023-01-04 10:36:35 -0500

    mina branch mina file

---
+++ src/Package01/Mina01.java
@@ -0,0 +1,10 @@
+package Package01;
+
+public class Mina01 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Git working flow practice");
+        //push to origin mina-feature branch
+    }
+}
